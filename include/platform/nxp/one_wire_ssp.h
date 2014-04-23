0a6cc37da6dc6bf4bdc447f733a27e3b1a40cc20
xent <alexdmitv@gmail.com>
2014-04-24 01:08:36 +0400

    Added initial version of OneWire over SSP. SSP is configured in 16-bit mode. MISO and MOSI pins are connected together, SCK and SSEL unused.

---
+++ include/platform/nxp/one_wire_ssp.h
@@ -0,0 +1,61 @@
+/*
+ * platform/nxp/one_wire_ssp.h
+ * Copyright (C) 2014 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#ifndef ONE_WIRE_SSP_H_
+#define ONE_WIRE_SSP_H_
+/*----------------------------------------------------------------------------*/
+#include <byte_queue.h>
+#include <irq.h>
+#include "ssp_base.h"
+/*----------------------------------------------------------------------------*/
+extern const struct InterfaceClass *OneWireSsp;
+/*----------------------------------------------------------------------------*/
+enum oneWireSspState
+{
+  OW_SSP_IDLE = 0,
+  OW_SSP_RESET,
+  OW_SSP_RECEIVE,
+  OW_SSP_TRANSMIT,
+  OW_SSP_ERROR
+};
+/*----------------------------------------------------------------------------*/
+struct OneWireSspConfig
+{
+  /** Mandatory: pin used for serial data reception from the bus. */
+  gpio_t miso;
+  /** Mandatory: pin used for data transmission on the serial bus. */
+  gpio_t mosi;
+  /** Optional: interrupt priority. */
+  priority_t priority;
+  /** Mandatory: peripheral identifier. */
+  uint8_t channel;
+};
+/*----------------------------------------------------------------------------*/
+struct OneWireSsp
+{
+  struct SspBase parent;
+
+  void (*callback)(void *);
+  void *callbackArgument;
+
+  /* Pointer to an input buffer */
+  uint8_t *rxBuffer;
+  /* Address of the device */
+  uint64_t address;
+  /* Number of bytes to be transmitted */
+  uint8_t left;
+  /* Position in a receiving word and temporary buffer for this word */
+  uint8_t bit, word;
+  /* Output queue containing command, address and data */
+  struct ByteQueue txQueue;
+
+  /* Current interface state */
+  enum oneWireSspState state;
+  /* Selection between blocking mode and zero copy mode */
+  bool blocking;
+};
+/*----------------------------------------------------------------------------*/
+#endif /* ONE_WIRE_SSP_H_ */
---
+++ platform/nxp/one_wire_ssp.c
@@ -0,0 +1,331 @@
+/*
+ * one_wire_ssp.c
+ * Copyright (C) 2014 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#include <memory.h>
+#include <platform/nxp/one_wire_ssp.h>
+#include <platform/nxp/ssp_defs.h>
+/*----------------------------------------------------------------------------*/
+#define RATE_RESET      16000
+#define RATE_DATA       260000
+
+#define MASK_DATA       0x3F00
+#define MASK_PRESENCE   0x00FF
+
+#define PATTERN_HIGH    0x7FFF
+#define PATTERN_LOW     0x000F
+#define PATTERN_RESET   0x00FF
+
+#define TX_QUEUE_LENGTH 24
+/*----------------------------------------------------------------------------*/
+enum oneWireRomCommand
+{
+  SEARCH_ROM  = 0xF0,
+  READ_ROM    = 0x33,
+  MATCH_ROM   = 0x55,
+  SKIP_ROM    = 0xCC
+};
+/*----------------------------------------------------------------------------*/
+static void adjustPins(struct OneWireSsp *, const struct OneWireSspConfig *);
+static void beginTransmission(struct OneWireSsp *);
+static void interruptHandler(void *);
+static void sendWord(struct OneWireSsp *, uint8_t);
+/*----------------------------------------------------------------------------*/
+static enum result oneWireInit(void *, const void *);
+static void oneWireDeinit(void *);
+static enum result oneWireCallback(void *, void (*)(void *), void *);
+static enum result oneWireGet(void *, enum ifOption, void *);
+static enum result oneWireSet(void *, enum ifOption, const void *);
+static uint32_t oneWireRead(void *, uint8_t *, uint32_t);
+static uint32_t oneWireWrite(void *, const uint8_t *, uint32_t);
+/*----------------------------------------------------------------------------*/
+static const struct InterfaceClass oneWireTable = {
+    .size = sizeof(struct OneWireSsp),
+    .init = oneWireInit,
+    .deinit = oneWireDeinit,
+
+    .callback = oneWireCallback,
+    .get = oneWireGet,
+    .set = oneWireSet,
+    .read = oneWireRead,
+    .write = oneWireWrite
+};
+/*----------------------------------------------------------------------------*/
+const struct InterfaceClass *OneWireSsp = &oneWireTable;
+/*----------------------------------------------------------------------------*/
+static void adjustPins(struct OneWireSsp *interface __attribute__((unused)),
+    const struct OneWireSspConfig *config)
+{
+  //FIXME
+  gpioSetType(gpioInit(config->mosi), GPIO_OPENDRAIN);
+  gpioSetPull(gpioInit(config->mosi), GPIO_PULLUP);
+  gpioSetPull(gpioInit(config->miso), GPIO_PULLUP);
+}
+/*----------------------------------------------------------------------------*/
+static void beginTransmission(struct OneWireSsp *interface)
+{
+  LPC_SSP_Type *reg = interface->parent.reg;
+
+  sspSetRate((struct SspBase *)interface, RATE_RESET);
+  interface->state = OW_SSP_RESET;
+  reg->DR = PATTERN_RESET; /* Execute reset */
+}
+/*----------------------------------------------------------------------------*/
+static void sendWord(struct OneWireSsp *interface, uint8_t word)
+{
+  LPC_SSP_Type *reg = interface->parent.reg;
+  uint8_t counter = 0;
+
+  while (counter < 8)
+    reg->DR = (word >> counter++) & 0x01 ? PATTERN_HIGH : PATTERN_LOW;
+}
+/*----------------------------------------------------------------------------*/
+static void interruptHandler(void *object)
+{
+  struct OneWireSsp *interface = object;
+  LPC_SSP_Type *reg = interface->parent.reg;
+  bool event = false;
+
+  while (reg->SR & SR_RNE)
+  {
+    uint16_t data = reg->DR;
+
+    switch (interface->state)
+    {
+      case OW_SSP_RESET:
+        if (data ^ MASK_PRESENCE)
+        {
+          sspSetRate((struct SspBase *)object, RATE_DATA);
+          interface->state = OW_SSP_TRANSMIT;
+        }
+        else
+        {
+          interface->state = OW_SSP_ERROR;
+          event = true;
+        }
+        break;
+
+      case OW_SSP_RECEIVE:
+        if (data & MASK_DATA)
+          interface->word |= 1 << interface->bit;
+
+      case OW_SSP_TRANSMIT:
+        if (++interface->bit == 8)
+        {
+          if (interface->state == OW_SSP_RECEIVE)
+          {
+            *interface->rxBuffer++ = interface->word;
+            interface->word = 0x00;
+          }
+          interface->bit = 0;
+          if (!--interface->left)
+          {
+            interface->state = OW_SSP_IDLE;
+            event = true;
+          }
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  if ((reg->SR & SR_TFE) && interface->state != OW_SSP_RESET)
+  {
+    /* Fill FIFO with next word or end the transaction */
+    if (!byteQueueEmpty(&interface->txQueue))
+      sendWord(interface, byteQueuePop(&interface->txQueue));
+  }
+
+  if (event)
+  {
+    reg->IMSC &= ~(IMSC_RXIM | IMSC_RTIM);
+
+    if (interface->callback)
+      interface->callback(interface->callbackArgument);
+  }
+}
+/*----------------------------------------------------------------------------*/
+static enum result oneWireInit(void *object, const void *configPtr)
+{
+  const struct OneWireSspConfig * const config = configPtr;
+  const struct SspBaseConfig parentConfig = {
+      .channel = config->channel,
+      .miso = config->miso,
+      .mosi = config->mosi,
+      .sck = 0,
+      .cs = 0
+  };
+  struct OneWireSsp *interface = object;
+  enum result res;
+
+  /* Call base class constructor */
+  if ((res = SspBase->init(object, &parentConfig)) != E_OK)
+    return res;
+
+  adjustPins(interface, config);
+
+  //TODO Rollback?
+  if ((res = byteQueueInit(&interface->txQueue, TX_QUEUE_LENGTH)) != E_OK)
+    return res;
+
+  interface->parent.handler = interruptHandler;
+
+  interface->address = 0;
+  interface->blocking = true;
+  interface->callback = 0;
+  interface->state = OW_SSP_IDLE;
+
+  LPC_SSP_Type *reg = interface->parent.reg;
+
+  /* Set frame size to 16 bit and SPI mode 0 */
+  reg->CR0 = CR0_FRF_TI | CR0_DSS(16);
+
+  sspSetRate(object, RATE_RESET);
+  /* Enable peripheral */
+  reg->CR1 = CR1_SSE;
+
+  irqSetPriority(interface->parent.irq, config->priority);
+  irqEnable(interface->parent.irq);
+
+  return E_OK;
+}
+/*----------------------------------------------------------------------------*/
+static void oneWireDeinit(void *object)
+{
+  struct OneWireSsp *interface = object;
+
+  irqDisable(interface->parent.irq);
+  byteQueueDeinit(&interface->txQueue);
+  SspBase->deinit(interface);
+}
+/*----------------------------------------------------------------------------*/
+static enum result oneWireCallback(void *object, void (*callback)(void *),
+    void *argument)
+{
+  struct OneWireSsp *interface = object;
+
+  interface->callbackArgument = argument;
+  interface->callback = callback;
+  return E_OK;
+}
+/*----------------------------------------------------------------------------*/
+static enum result oneWireGet(void *object, enum ifOption option,
+    void *data __attribute__((unused)))
+{
+  struct OneWireSsp *interface = object;
+
+  switch (option)
+  {
+    case IF_STATUS:
+      if (!interface->blocking && interface->state == OW_SSP_ERROR)
+        return E_ERROR;
+      else
+        return interface->state != OW_SSP_IDLE ? E_BUSY : E_OK;
+    default:
+      return E_ERROR;
+  }
+}
+/*----------------------------------------------------------------------------*/
+static enum result oneWireSet(void *object, enum ifOption option,
+    const void *data)
+{
+  struct OneWireSsp *interface = object;
+
+  switch (option)
+  {
+    case IF_BLOCKING:
+      interface->blocking = true;
+      return E_OK;
+    case IF_DEVICE:
+      //TODO Add byte order conversions
+      interface->address = *(uint64_t *)data;
+      return E_OK;
+    case IF_ZEROCOPY:
+      interface->blocking = false;
+      return E_OK;
+    default:
+      return E_ERROR;
+  }
+}
+/*----------------------------------------------------------------------------*/
+static uint32_t oneWireRead(void *object, uint8_t *buffer, uint32_t length)
+{
+  struct OneWireSsp *interface = object;
+  LPC_SSP_Type *reg = interface->parent.reg;
+  uint32_t read = 0;
+
+  if (!length)
+    return 0;
+
+  byteQueueClear(&interface->txQueue);
+  interface->bit = 0;
+  interface->rxBuffer = buffer;
+  interface->word = 0x00;
+  while (!byteQueueFull(&interface->txQueue) && ++read != length)
+    byteQueuePush(&interface->txQueue, 0xFF);
+  interface->left = read;
+
+  interface->state = OW_SSP_RECEIVE;
+
+  /* Clear interrupt flags, enable interrupts and start reception */
+  reg->ICR = ICR_RORIC | ICR_RTIC;
+  reg->IMSC |= IMSC_RXIM | IMSC_RTIM;
+  sendWord(interface, 0xFF);
+
+  if (interface->blocking)
+  {
+    while (interface->state != OW_SSP_IDLE && interface->state != OW_SSP_ERROR)
+      barrier();
+
+    if (interface->state == OW_SSP_ERROR)
+      return 0;
+  }
+
+  return read;
+}
+/*----------------------------------------------------------------------------*/
+static uint32_t oneWireWrite(void *object, const uint8_t *buffer,
+    uint32_t length)
+{
+  struct OneWireSsp *interface = object;
+  LPC_SSP_Type *reg = interface->parent.reg;
+  uint32_t written;
+
+  if (!length)
+    return 0;
+
+  byteQueueClear(&interface->txQueue);
+  interface->bit = 0;
+  interface->left = 1;
+  /* Initiate new transaction by selecting the addressing mode */
+  if (interface->address)
+  {
+    byteQueuePush(&interface->txQueue, (uint8_t)MATCH_ROM);
+    interface->left += byteQueuePushArray(&interface->txQueue,
+        (const uint8_t *)&interface->address, length);
+  }
+  else
+    byteQueuePush(&interface->txQueue, (uint8_t)SKIP_ROM);
+  written = byteQueuePushArray(&interface->txQueue, buffer, length);
+  interface->left += written;
+
+  /* Clear interrupt flags, enable interrupts and start transmission */
+  reg->ICR = ICR_RORIC | ICR_RTIC;
+  reg->IMSC |= IMSC_RXIM | IMSC_RTIM;
+  beginTransmission(interface);
+
+  if (interface->blocking)
+  {
+    while (interface->state != OW_SSP_IDLE && interface->state != OW_SSP_ERROR)
+      barrier();
+
+    if (interface->state == OW_SSP_ERROR)
+      return 0;
+  }
+
+  return written;
+}
