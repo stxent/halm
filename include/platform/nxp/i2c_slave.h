0feb467e31f9982d7fe49ccb734967fa9a8b5224
xent <alexdmitv@gmail.com>
2014-05-07 00:41:08 +0400

    Added simple I2C slave driver.

---
+++ include/platform/nxp/i2c_slave.h
@@ -0,0 +1,56 @@
+/*
+ * platform/nxp/i2c_slave.h
+ * Copyright (C) 2014 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#ifndef I2C_SLAVE_H_
+#define I2C_SLAVE_H_
+/*----------------------------------------------------------------------------*/
+#include <stdbool.h>
+#include "i2c_base.h"
+/*----------------------------------------------------------------------------*/
+extern const struct InterfaceClass *I2cSlave;
+/*----------------------------------------------------------------------------*/
+enum i2cSlaveState
+{
+  I2C_SLAVE_IDLE = 0,
+  I2C_SLAVE_ADDRESS,
+  I2C_SLAVE_DATA
+};
+/*----------------------------------------------------------------------------*/
+struct I2cSlaveConfig
+{
+  /** Mandatory: register map size. */
+  uint16_t size;
+  /** Mandatory: serial clock pin. */
+  gpio_t scl;
+  /** Mandatory: serial data pin. */
+  gpio_t sda;
+  /** Optional: interrupt priority. */
+  priority_t priority;
+  /** Mandatory: peripheral identifier. */
+  uint8_t channel;
+};
+/*----------------------------------------------------------------------------*/
+struct I2cSlave
+{
+  struct I2cBase parent;
+
+  void (*callback)(void *);
+  void *callbackArgument;
+
+  /* Register cache */
+  uint8_t *cache;
+  /* Cache size */
+  uint16_t size;
+  /* Position in register cache for external access */
+  uint16_t external;
+  /* Position in register cache for internal operations */
+  uint16_t internal;
+
+  /* Current interface state */
+  enum i2cSlaveState state;
+};
+/*----------------------------------------------------------------------------*/
+#endif /* I2C_SLAVE_H_ */
---
+++ platform/nxp/i2c_slave.c
@@ -0,0 +1,252 @@
+/*
+ * i2c_slave.c
+ * Copyright (C) 2014 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <memory.h>
+#include <platform/nxp/i2c_slave.h>
+#include <platform/nxp/i2c_defs.h>
+/*----------------------------------------------------------------------------*/
+/* Slave receiver and transmitter modes except general call modes */
+enum status
+{
+  STAT_ADDRESS_WRITE_RECEIVED = 0x60,
+  STAT_DATA_RECEIVED_ACK      = 0x80,
+  STAT_DATA_RECEIVED_NACK     = 0x88,
+  STAT_STOP_RECEIVED          = 0xA0,
+  STAT_ADDRESS_READ_RECEIVED  = 0xA8,
+  STAT_DATA_TRANSMITTED_ACK   = 0xB8,
+  STAT_DATA_TRANSMITTED_NACK  = 0xC0,
+  STAT_LAST_TRANSMITTED_ACK   = 0xC8
+};
+/*----------------------------------------------------------------------------*/
+static void interruptHandler(void *);
+/*----------------------------------------------------------------------------*/
+static enum result i2cInit(void *, const void *);
+static void i2cDeinit(void *);
+static enum result i2cCallback(void *, void (*)(void *), void *);
+static enum result i2cGet(void *, enum ifOption, void *);
+static enum result i2cSet(void *, enum ifOption, const void *);
+static uint32_t i2cRead(void *, uint8_t *, uint32_t);
+static uint32_t i2cWrite(void *, const uint8_t *, uint32_t);
+/*----------------------------------------------------------------------------*/
+static const struct InterfaceClass i2cTable = {
+    .size = sizeof(struct I2cSlave),
+    .init = i2cInit,
+    .deinit = i2cDeinit,
+
+    .callback = i2cCallback,
+    .get = i2cGet,
+    .set = i2cSet,
+    .read = i2cRead,
+    .write = i2cWrite
+};
+/*----------------------------------------------------------------------------*/
+const struct InterfaceClass *I2cSlave = &i2cTable;
+/*----------------------------------------------------------------------------*/
+static void interruptHandler(void *object)
+{
+  struct I2cSlave *interface = object;
+  LPC_I2C_Type *reg = interface->parent.reg;
+  bool event = false;
+
+  switch (reg->STAT)
+  {
+    case STAT_ADDRESS_WRITE_RECEIVED:
+      interface->state = I2C_SLAVE_ADDRESS;
+      break;
+
+    case STAT_ADDRESS_READ_RECEIVED:
+      reg->DAT = interface->cache[interface->external++];
+
+      /* Wrap current external position after reaching the end of cache */
+      if (interface->external >= interface->size)
+        interface->external = 0;
+
+      interface->state = I2C_SLAVE_DATA;
+      break;
+
+    case STAT_DATA_RECEIVED_ACK:
+      if (interface->state == I2C_SLAVE_ADDRESS)
+      {
+        interface->external = reg->DAT;
+        interface->state = I2C_SLAVE_DATA;
+      }
+      else
+      {
+        interface->cache[interface->external++] = reg->DAT;
+
+        if (interface->external >= interface->size)
+          interface->external = 0;
+      }
+      break;
+
+    case STAT_DATA_TRANSMITTED_ACK:
+      reg->DAT = interface->cache[interface->external++];
+
+      if (interface->external >= interface->size)
+        interface->external = 0;
+      break;
+
+    case STAT_DATA_RECEIVED_NACK:
+    case STAT_STOP_RECEIVED:
+    case STAT_DATA_TRANSMITTED_NACK:
+    case STAT_LAST_TRANSMITTED_ACK:
+      interface->state = I2C_SLAVE_IDLE;
+      event = true;
+      break;
+
+    default:
+      break;
+  }
+
+  reg->CONCLR = CONCLR_SIC;
+  reg->CONSET = CONSET_AA;
+
+  if (interface->callback && event)
+    interface->callback(interface->callbackArgument);
+}
+/*----------------------------------------------------------------------------*/
+static enum result i2cInit(void *object, const void *configPtr)
+{
+  const struct I2cSlaveConfig * const config = configPtr;
+  const struct I2cBaseConfig parentConfig = {
+      .channel = config->channel,
+      .scl = config->scl,
+      .sda = config->sda
+  };
+  struct I2cSlave *interface = object;
+  enum result res;
+
+  /* Call base class constructor */
+  if ((res = I2cBase->init(object, &parentConfig)) != E_OK)
+    return res;
+
+  interface->cache = malloc(config->size);
+  interface->size = config->size;
+
+  interface->callback = 0;
+  interface->external = 0;
+  interface->internal = 0;
+  interface->state = I2C_SLAVE_IDLE;
+
+  interface->parent.handler = interruptHandler;
+
+  LPC_I2C_Type *reg = interface->parent.reg;
+
+  /* Clear all flags */
+  reg->CONCLR = CONCLR_AAC | CONCLR_SIC | CONCLR_STAC | CONCLR_I2ENC;
+
+  irqSetPriority(interface->parent.irq, config->priority);
+  irqEnable(interface->parent.irq);
+
+  /* Enable I2C interface */
+  reg->CONSET = CONSET_AA | CONSET_I2EN;
+
+  return E_OK;
+}
+/*----------------------------------------------------------------------------*/
+static void i2cDeinit(void *object)
+{
+  struct I2cSlave *interface = object;
+  LPC_I2C_Type *reg = interface->parent.reg;
+
+  reg->CONCLR = CONCLR_I2ENC; /* Disable I2C interface */
+
+  free(interface->cache);
+  I2cBase->deinit(interface);
+}
+/*----------------------------------------------------------------------------*/
+static enum result i2cCallback(void *object, void (*callback)(void *),
+    void *argument)
+{
+  struct I2cSlave *interface = object;
+
+  interface->callbackArgument = argument;
+  interface->callback = callback;
+  return E_OK;
+}
+/*----------------------------------------------------------------------------*/
+static enum result i2cGet(void *object, enum ifOption option, void *data)
+{
+  struct I2cSlave *interface = object;
+  LPC_I2C_Type *reg = interface->parent.reg;
+
+  switch (option)
+  {
+    case IF_ADDRESS:
+      *(uint32_t *)data = interface->internal;
+      return E_OK;
+    case IF_DEVICE:
+       *(uint32_t *)data = reg->ADR0 >> 1; //FIXME
+      return E_OK;
+    case IF_STATUS:
+      return interface->state != I2C_SLAVE_IDLE ? E_BUSY : E_OK;
+    default:
+      return E_ERROR;
+  }
+}
+/*----------------------------------------------------------------------------*/
+static enum result i2cSet(void *object, enum ifOption option, const void *data)
+{
+  struct I2cSlave *interface = object;
+  LPC_I2C_Type *reg = interface->parent.reg;
+
+  switch (option)
+  {
+    case IF_ADDRESS:
+      interface->internal = *(uint32_t *)data;
+      return E_OK;
+    case IF_DEVICE:
+      reg->ADR0 = *(uint32_t *)data << 1; //FIXME
+      return E_OK;
+    default:
+      return E_ERROR;
+  }
+}
+/*----------------------------------------------------------------------------*/
+static uint32_t i2cRead(void *object, uint8_t *buffer, uint32_t length)
+{
+  struct I2cSlave *interface = object;
+  uint32_t read;
+
+  if (!length)
+    return 0;
+
+  read = interface->internal + length <= interface->size ? length
+      : interface->size - interface->internal;
+
+  memcpy(buffer, interface->cache + interface->internal, read);
+  interface->internal += read;
+
+  /* Wrap address */
+  if (interface->internal >= interface->size)
+    interface->internal = 0;
+
+  return read;
+}
+/*----------------------------------------------------------------------------*/
+static uint32_t i2cWrite(void *object, const uint8_t *buffer, uint32_t length)
+{
+  struct I2cSlave *interface = object;
+  uint32_t written;
+
+  if (!length)
+    return 0;
+
+  written = interface->internal + length <= interface->size ? length
+      : interface->size - interface->internal;
+
+  memcpy(interface->cache + interface->internal, buffer, written);
+  interface->internal += written;
+
+  /* Wrap address */
+  if (interface->internal >= interface->size)
+    interface->internal = 0;
+
+  return written;
+}
