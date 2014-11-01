1f23f63674883ba877478441a29f417dc698e6b0
xent <alexdmitv@gmail.com>
2014-11-01 20:12:30 +0300

    Added simple timer based on SCT peripheral.

---
+++ include/platform/nxp/sct_timer.h
@@ -0,0 +1,44 @@
+/*
+ * platform/nxp/sct_timer.h
+ * Copyright (C) 2014 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#ifndef PLATFORM_NXP_SCT_TIMER_H_
+#define PLATFORM_NXP_SCT_TIMER_H_
+/*----------------------------------------------------------------------------*/
+#include "sct_base.h"
+/*----------------------------------------------------------------------------*/
+extern const struct TimerClass * const SctTimer;
+/*----------------------------------------------------------------------------*/
+struct SctTimerConfig
+{
+  /**
+   * Optional: desired timer tick rate. An actual peripheral frequency is used
+   * when option is set to zero.
+   */
+  uint32_t frequency;
+  /** Optional: timer interrupt priority. */
+  priority_t priority;
+  /** Mandatory: peripheral identifier. */
+  uint8_t channel;
+  /** Optional: do not enable the timer after the initialization. */
+  bool disabled;
+  /** Optional: timer part. */
+  enum sctPart part;
+};
+/*----------------------------------------------------------------------------*/
+struct SctTimer
+{
+  struct SctBase parent;
+
+  void (*callback)(void *);
+  void *callbackArgument;
+
+  /* Desired timer frequency */
+  uint32_t frequency;
+  /* Match channel used for counter reset */
+  uint8_t event;
+};
+/*----------------------------------------------------------------------------*/
+#endif /* PLATFORM_NXP_SCT_TIMER_H_ */
---
+++ platform/nxp/sct_timer.c
@@ -0,0 +1,247 @@
+/*
+ * sct_timer.c
+ * Copyright (C) 2014 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#include <platform/platform_defs.h>
+#include <platform/nxp/sct_defs.h>
+#include <platform/nxp/sct_timer.h>
+/*----------------------------------------------------------------------------*/
+static void interruptHandler(void *);
+static enum result updateFrequency(struct SctTimer *, uint32_t);
+/*----------------------------------------------------------------------------*/
+static enum result tmrInit(void *, const void *);
+static void tmrDeinit(void *);
+static void tmrCallback(void *, void (*)(void *), void *);
+static void tmrSetEnabled(void *, bool);
+static enum result tmrSetFrequency(void *, uint32_t);
+static enum result tmrSetOverflow(void *, uint32_t);
+static enum result tmrSetValue(void *, uint32_t);
+static uint32_t tmrValue(const void *);
+/*----------------------------------------------------------------------------*/
+static const struct TimerClass timerTable = {
+    .size = sizeof(struct SctTimer),
+    .init = tmrInit,
+    .deinit = tmrDeinit,
+
+    .callback = tmrCallback,
+    .setEnabled = tmrSetEnabled,
+    .setFrequency = tmrSetFrequency,
+    .setOverflow = tmrSetOverflow,
+    .setValue = tmrSetValue,
+    .value = tmrValue
+};
+/*----------------------------------------------------------------------------*/
+const struct TimerClass * const SctTimer = &timerTable;
+/*----------------------------------------------------------------------------*/
+static void interruptHandler(void *object)
+{
+  struct SctTimer * const timer = object;
+
+  if (timer->callback)
+    timer->callback(timer->callbackArgument);
+}
+/*----------------------------------------------------------------------------*/
+static enum result updateFrequency(struct SctTimer *timer, uint32_t frequency)
+{
+  LPC_SCT_Type * const reg = timer->parent.reg;
+  const uint8_t offset = timer->parent.part == SCT_HIGH;
+  const uint32_t value = (reg->CTRL_PART[offset] & ~CTRL_PRE_MASK)
+      | CTRL_CLRCTR;
+
+  if (frequency)
+  {
+    /* TODO Check whether the clock is from internal source */
+    const uint32_t baseClock = sctGetClock((struct SctBase *)timer);
+    const uint16_t prescaler = baseClock / frequency - 1;
+
+    if (prescaler >= 256)
+      return E_VALUE;
+
+    reg->CTRL_PART[offset] = value | CTRL_PRE(prescaler);
+  }
+  else
+    reg->CTRL_PART[offset] = value;
+
+  timer->frequency = frequency;
+  return E_OK;
+}
+/*----------------------------------------------------------------------------*/
+static enum result tmrInit(void *object, const void *configBase)
+{
+  const struct SctTimerConfig * const config = configBase;
+  const struct SctBaseConfig parentConfig = {
+      .channel = config->channel,
+      .input = SCT_INPUT_NONE,
+      .part = config->part
+  };
+  struct SctTimer * const timer = object;
+  enum result res;
+
+  /* Call base class constructor */
+  if ((res = SctBase->init(object, &parentConfig)) != E_OK)
+    return res;
+
+  const int8_t event = sctAllocateEvent((struct SctBase *)timer);
+
+  if (event == -1)
+    return E_BUSY;
+
+  timer->parent.handler = interruptHandler;
+  timer->callback = 0;
+  timer->event = (uint8_t)event;
+
+  LPC_SCT_Type * const reg = timer->parent.reg;
+  const uint16_t eventMask = 1 << timer->event;
+  const uint8_t offset = timer->parent.part == SCT_HIGH;
+
+  timer->parent.mask = eventMask;
+  reg->CTRL_PART[offset] = CTRL_HALT;
+  reg->CONFIG |= CONFIG_NORELOAD(offset);
+
+  /* Set desired timer frequency */
+  if ((res = updateFrequency(timer, config->frequency)) != E_OK)
+    return res;
+
+  /* Set match register value depending on timer type */
+  if (timer->parent.part == SCT_UNIFIED)
+    reg->MATCH[0] = 0xFFFFFFFF;
+  else
+    reg->MATCH_PART[0][offset] = 0xFFFF;
+
+  /* Configure event */
+  reg->EV[timer->event].CTRL = EVCTRL_MATCHSEL(0)
+      | EVCTRL_COMBMODE(EVCTRL_COMBMODE_MATCH) | EVCTRL_MATCHMEM
+      | EVCTRL_DIRECTION(EVCTRL_DIRECTION_IND);
+  if (timer->parent.part == SCT_HIGH)
+    reg->EV[timer->event].CTRL |= EVCTRL_HEVENT;
+
+  /* Enable allocated event in state 0 */
+  reg->EV[timer->event].STATE = 0x00000001;
+  /* Enable timer clearing on allocated event */
+  reg->LIMIT_PART[offset] = eventMask;
+  /* Disable interrupt requests when no callback is specified */
+  reg->EVEN &= ~eventMask;
+
+  /* TODO Clear unused registers */
+
+  if (!config->disabled)
+    reg->CTRL_PART[offset] &= ~CTRL_HALT;
+
+  return E_OK;
+}
+/*----------------------------------------------------------------------------*/
+static void tmrDeinit(void *object)
+{
+  struct SctTimer * const timer = object;
+
+  SctBase->deinit(timer);
+}
+/*----------------------------------------------------------------------------*/
+static void tmrCallback(void *object, void (*callback)(void *), void *argument)
+{
+  struct SctTimer * const timer = object;
+  LPC_SCT_Type * const reg = timer->parent.reg;
+  const uint16_t eventMask = timer->parent.mask;
+
+  timer->callbackArgument = argument;
+  timer->callback = callback;
+
+  if (callback)
+  {
+    /* Clear pending requests */
+    reg->EVFLAG = eventMask;
+    /* Enable interrupt requests */
+    reg->EVEN |= eventMask;
+  }
+  else
+    reg->EVEN &= eventMask;
+}
+/*----------------------------------------------------------------------------*/
+static void tmrSetEnabled(void *object, bool state)
+{
+  struct SctTimer * const timer = object;
+  LPC_SCT_Type * const reg = timer->parent.reg;
+  const uint8_t offset = timer->parent.part == SCT_HIGH;
+
+  if (state)
+  {
+    reg->EVFLAG = timer->parent.mask;
+    reg->CTRL_PART[offset] &= ~CTRL_HALT;
+  }
+  else
+    reg->CTRL_PART[offset] |= CTRL_HALT;
+}
+/*----------------------------------------------------------------------------*/
+static enum result tmrSetFrequency(void *object, uint32_t frequency)
+{
+  struct SctTimer * const timer = object;
+
+  return updateFrequency(timer, frequency);
+}
+/*----------------------------------------------------------------------------*/
+static enum result tmrSetOverflow(void *object, uint32_t overflow)
+{
+  struct SctTimer * const timer = object;
+  LPC_SCT_Type * const reg = timer->parent.reg;
+  const uint8_t offset = timer->parent.part == SCT_HIGH;
+  enum result res = E_VALUE;
+
+  reg->CTRL_PART[offset] |= CTRL_STOP;
+  if (timer->parent.part == SCT_UNIFIED)
+  {
+    reg->MATCH[0] = overflow - 1;
+    res = E_OK;
+  }
+  else
+  {
+    if (overflow <= 0x10000)
+    {
+      reg->MATCH_PART[0][offset] = overflow - 1;
+      res = E_OK;
+    }
+  }
+  reg->CTRL_PART[offset] &= ~CTRL_STOP;
+
+  return res;
+}
+/*----------------------------------------------------------------------------*/
+static enum result tmrSetValue(void *object, uint32_t value)
+{
+  struct SctTimer * const timer = object;
+  LPC_SCT_Type * const reg = timer->parent.reg;
+  const uint8_t offset = timer->parent.part == SCT_HIGH;
+  enum result res = E_VALUE;
+
+  reg->CTRL_PART[offset] |= CTRL_STOP;
+  if (timer->parent.part == SCT_UNIFIED)
+  {
+    if (value <= reg->MATCH[0])
+    {
+      reg->COUNT = value;
+      res = E_OK;
+    }
+  }
+  else
+  {
+    if (value <= reg->COUNT_PART[offset])
+    {
+      reg->COUNT_PART[offset] = value;
+      res = E_OK;
+    }
+  }
+  reg->CTRL_PART[offset] &= ~CTRL_STOP;
+
+  return res;
+}
+/*----------------------------------------------------------------------------*/
+static uint32_t tmrValue(const void *object)
+{
+  const struct SctTimer * const timer = object;
+  const LPC_SCT_Type * const reg = timer->parent.reg;
+  const uint8_t offset = timer->parent.part == SCT_HIGH;
+
+  return timer->parent.part == SCT_UNIFIED ? reg->COUNT
+      : reg->COUNT_PART[offset];
+}
