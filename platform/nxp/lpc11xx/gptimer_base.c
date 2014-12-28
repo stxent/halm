8d0784f38f8485e64a20e2b38120727b055efdcc
xent <alexdmitv@gmail.com>
2014-12-28 19:17:05 +0300

    Added missing modules for LPC11xx. Added Pin Interrupt module for LPC11xx and LPC11Exx. Added GpTimer base class for LPC11xx.

---
+++ platform/nxp/lpc11xx/gptimer_base.c
@@ -0,0 +1,205 @@
+/*
+ * gptimer_base.c
+ * Copyright (C) 2014 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#include <assert.h>
+#include <memory.h>
+#include <platform/nxp/gptimer_base.h>
+#include <platform/nxp/lpc11xx/clocking.h>
+#include <platform/nxp/lpc11xx/system.h>
+/*----------------------------------------------------------------------------*/
+/* Pack capture or match channel and pin function in one value */
+#define PACK_VALUE(function, channel) (((channel) << 4) | (function))
+/*----------------------------------------------------------------------------*/
+struct TimerBlockDescriptor
+{
+  LPC_TIMER_Type *reg;
+  uint8_t resolution;
+  enum sysClockDevice clock;
+};
+/*----------------------------------------------------------------------------*/
+static enum result setDescriptor(uint8_t, const struct GpTimerBase *,
+    struct GpTimerBase *);
+/*----------------------------------------------------------------------------*/
+static enum result tmrInit(void *, const void *);
+static void tmrDeinit(void *);
+/*----------------------------------------------------------------------------*/
+static const struct EntityClass timerTable = {
+    .size = 0, /* Abstract class */
+    .init = tmrInit,
+    .deinit = tmrDeinit
+};
+/*----------------------------------------------------------------------------*/
+static const struct TimerBlockDescriptor timerBlockEntries[4] = {
+    {
+        .reg = LPC_TIMER16B0,
+        .resolution = 16,
+        .clock = CLK_CT16B0
+    },
+    {
+        .reg = LPC_TIMER16B1,
+        .resolution = 16,
+        .clock = CLK_CT16B1
+    },
+    {
+        .reg = LPC_TIMER32B0,
+        .resolution = 32,
+        .clock = CLK_CT32B0
+    },
+    {
+        .reg = LPC_TIMER32B1,
+        .resolution = 32,
+        .clock = CLK_CT32B1
+    }
+};
+/*----------------------------------------------------------------------------*/
+const struct PinEntry gpTimerCapturePins[] = {
+    {
+        .key = PIN(0, 2), /* CT16B0_CAP0 */
+        .channel = GPTIMER_CT16B0,
+        .value = PACK_VALUE(2, 0)
+    }, {
+        .key = PIN(1, 0), /* CT32B1_CAP0 */
+        .channel = GPTIMER_CT32B1,
+        .value = PACK_VALUE(3, 0)
+    }, {
+        .key = PIN(1, 5), /* CT32B0_CAP0 */
+        .channel = GPTIMER_CT32B0,
+        .value = PACK_VALUE(2, 0)
+    }, {
+        .key = PIN(1, 8), /* CT16B1_CAP0 */
+        .channel = GPTIMER_CT16B1,
+        .value = PACK_VALUE(1, 0)
+    }, {
+        .key = 0 /* End of pin function association list */
+    }
+};
+/*----------------------------------------------------------------------------*/
+const struct PinEntry gpTimerMatchPins[] = {
+    {
+        .key = PIN(0, 1), /* CT32B0_MAT2 */
+        .channel = GPTIMER_CT32B0,
+        .value = PACK_VALUE(2, 2)
+    }, {
+        .key = PIN(0, 8), /* CT16B0_MAT0 */
+        .channel = GPTIMER_CT16B0,
+        .value = PACK_VALUE(2, 0)
+    }, {
+        .key = PIN(0, 9), /* CT16B0_MAT1 */
+        .channel = GPTIMER_CT16B0,
+        .value = PACK_VALUE(2, 1)
+    }, {
+        .key = PIN(0, 10), /* CT16B0_MAT2 */
+        .channel = GPTIMER_CT16B0,
+        .value = PACK_VALUE(3, 2)
+    }, {
+        .key = PIN(0, 11), /* CT32B0_MAT3 */
+        .channel = GPTIMER_CT32B0,
+        .value = PACK_VALUE(3, 3)
+    }, {
+        .key = PIN(1, 1), /* CT32B1_MAT0 */
+        .channel = GPTIMER_CT32B1,
+        .value = PACK_VALUE(3, 0)
+    }, {
+        .key = PIN(1, 2), /* CT32B1_MAT1 */
+        .channel = GPTIMER_CT32B1,
+        .value = PACK_VALUE(3, 1)
+    }, {
+        .key = PIN(1, 3), /* CT32B1_MAT2 */
+        .channel = GPTIMER_CT32B1,
+        .value = PACK_VALUE(3, 2)
+    }, {
+        .key = PIN(1, 4), /* CT32B1_MAT3 */
+        .channel = GPTIMER_CT32B1,
+        .value = PACK_VALUE(2, 3)
+    }, {
+        .key = PIN(1, 6), /* CT32B0_MAT0 */
+        .channel = GPTIMER_CT32B0,
+        .value = PACK_VALUE(2, 0)
+    }, {
+        .key = PIN(1, 7), /* CT32B0_MAT1 */
+        .channel = GPTIMER_CT32B0,
+        .value = PACK_VALUE(2, 1)
+    }, {
+        .key = PIN(1, 9), /* CT16B1_MAT0 */
+        .channel = GPTIMER_CT16B1,
+        .value = PACK_VALUE(1, 0)
+    }, {
+        .key = PIN(1, 10), /* CT16B1_MAT1 */
+        .channel = GPTIMER_CT16B1,
+        .value = PACK_VALUE(2, 1)
+    }, {
+        .key = 0 /* End of pin function association list */
+    }
+};
+/*----------------------------------------------------------------------------*/
+const struct EntityClass * const GpTimerBase = &timerTable;
+static struct GpTimerBase *descriptors[4] = {0};
+/*----------------------------------------------------------------------------*/
+static enum result setDescriptor(uint8_t channel,
+    const struct GpTimerBase *state, struct GpTimerBase *timer)
+{
+  assert(channel < ARRAY_SIZE(descriptors));
+
+  return compareExchangePointer((void **)(descriptors + channel), state,
+      timer) ? E_OK : E_BUSY;
+}
+/*----------------------------------------------------------------------------*/
+void TIMER16B0_ISR(void)
+{
+  descriptors[0]->handler(descriptors[0]);
+}
+/*----------------------------------------------------------------------------*/
+void TIMER16B1_ISR(void)
+{
+  descriptors[1]->handler(descriptors[1]);
+}
+/*----------------------------------------------------------------------------*/
+void TIMER32B0_ISR(void)
+{
+  descriptors[2]->handler(descriptors[2]);
+}
+/*----------------------------------------------------------------------------*/
+void TIMER32B1_ISR(void)
+{
+  descriptors[3]->handler(descriptors[3]);
+}
+/*----------------------------------------------------------------------------*/
+uint32_t gpTimerGetClock(const struct GpTimerBase *timer
+    __attribute__((unused)))
+{
+  return clockFrequency(MainClock);
+}
+/*----------------------------------------------------------------------------*/
+static enum result tmrInit(void *object, const void *configBase)
+{
+  const struct GpTimerBaseConfig * const config = configBase;
+  struct GpTimerBase * const timer = object;
+  enum result res;
+
+  /* Try to set peripheral descriptor */
+  timer->channel = config->channel;
+  if ((res = setDescriptor(timer->channel, 0, timer)) != E_OK)
+    return res;
+
+  const struct TimerBlockDescriptor *entry = &timerBlockEntries[timer->channel];
+
+  sysClockEnable(entry->clock);
+
+  timer->handler = 0;
+  timer->irq = TIMER16B0_IRQ + timer->channel;
+  timer->reg = entry->reg;
+  timer->resolution = entry->resolution;
+
+  return E_OK;
+}
+/*----------------------------------------------------------------------------*/
+static void tmrDeinit(void *object)
+{
+  const struct GpTimerBase * const timer = object;
+
+  sysClockDisable(timerBlockEntries[timer->channel].clock);
+  setDescriptor(timer->channel, timer, 0);
+}
---
+++ platform/nxp/lpc11xx/pin_interrupt.c
@@ -0,0 +1,257 @@
+/*
+ * pin_interrupt.c
+ * Copyright (C) 2014 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#include <assert.h>
+#include <irq.h>
+#include <list.h>
+#include <platform/nxp/pin_interrupt.h>
+/*----------------------------------------------------------------------------*/
+struct PinInterruptHandler
+{
+  struct Entity parent;
+
+  struct List list;
+};
+/*----------------------------------------------------------------------------*/
+static inline LPC_GPIO_Type *calcPort(uint8_t);
+static inline irq_t calcVector(uint8_t);
+static void processInterrupt(uint8_t);
+/*----------------------------------------------------------------------------*/
+static enum result pinInterruptHandlerAttach(union PinData,
+    const struct PinInterrupt *);
+static void pinInterruptHandlerDetach(const struct PinInterrupt *);
+static enum result pinInterruptHandlerInit(void *, const void *);
+/*----------------------------------------------------------------------------*/
+static enum result pinInterruptInit(void *, const void *);
+static void pinInterruptDeinit(void *);
+static void pinInterruptCallback(void *, void (*)(void *), void *);
+static void pinInterruptSetEnabled(void *, bool);
+/*----------------------------------------------------------------------------*/
+static const struct EntityClass handlerTable = {
+    .size = sizeof(struct PinInterruptHandler),
+    .init = pinInterruptHandlerInit,
+    .deinit = 0
+};
+/*----------------------------------------------------------------------------*/
+static const struct InterruptClass pinInterruptTable = {
+    .size = sizeof(struct PinInterrupt),
+    .init = pinInterruptInit,
+    .deinit = pinInterruptDeinit,
+
+    .callback = pinInterruptCallback,
+    .setEnabled = pinInterruptSetEnabled
+};
+/*----------------------------------------------------------------------------*/
+static const struct EntityClass * const PinInterruptHandler = &handlerTable;
+const struct InterruptClass * const PinInterrupt = &pinInterruptTable;
+static struct PinInterruptHandler *handlers[4] = {0};
+/*----------------------------------------------------------------------------*/
+static inline LPC_GPIO_Type *calcPort(uint8_t port)
+{
+  return (LPC_GPIO_Type *)((uint32_t)LPC_GPIO0 +
+      ((uint32_t)LPC_GPIO1 - (uint32_t)LPC_GPIO0) * port);
+}
+/*----------------------------------------------------------------------------*/
+static inline irq_t calcVector(uint8_t port)
+{
+  return PIOINT0_IRQ - port;
+}
+/*----------------------------------------------------------------------------*/
+static void processInterrupt(uint8_t channel)
+{
+  const struct List * const list = &handlers[channel]->list;
+  const struct ListNode *current = listFirst(list);
+  LPC_GPIO_Type * const reg = calcPort(channel);
+  const uint32_t state = reg->MIS;
+  struct PinInterrupt *interrupt;
+
+  /* Synchronizer logic causes a delay of 2 clocks */
+  reg->IC = state;
+
+  while (current)
+  {
+    listData(list, current, &interrupt);
+
+    if (state & (1 << interrupt->pin.offset))
+    {
+      if (interrupt->callback)
+        interrupt->callback(interrupt->callbackArgument);
+      break;
+    }
+
+    current = listNext(current);
+  }
+}
+/*----------------------------------------------------------------------------*/
+void PIO0_ISR(void)
+{
+  processInterrupt(0);
+}
+/*----------------------------------------------------------------------------*/
+void PIO1_ISR(void)
+{
+  processInterrupt(1);
+}
+/*----------------------------------------------------------------------------*/
+void PIO2_ISR(void)
+{
+  processInterrupt(2);
+}
+/*----------------------------------------------------------------------------*/
+void PIO3_ISR(void)
+{
+  processInterrupt(3);
+}
+/*----------------------------------------------------------------------------*/
+static enum result pinInterruptHandlerAttach(union PinData pin,
+    const struct PinInterrupt *interrupt)
+{
+  if (!handlers[pin.port])
+    handlers[pin.port] = init(PinInterruptHandler, 0);
+
+  assert(handlers[pin.port]);
+
+  struct List * const list = &handlers[pin.port]->list;
+  const struct ListNode *current = listFirst(list);
+  struct PinInterrupt *entry;
+
+  /* Check for duplicates */
+  while (current)
+  {
+    listData(list, current, &entry);
+
+    if (entry->pin.key == pin.key)
+      return E_BUSY;
+
+    current = listNext(current);
+  }
+
+  /* Add to list */
+  const bool empty = listEmpty(list);
+  const enum result res = listPush(list, &interrupt);
+
+  if (res == E_OK && empty)
+    irqEnable(calcVector(pin.port));
+
+  return res;
+}
+/*----------------------------------------------------------------------------*/
+static void pinInterruptHandlerDetach(const struct PinInterrupt *interrupt)
+{
+  struct List * const list = &handlers[interrupt->pin.port]->list;
+  struct ListNode *current = listFirst(list);
+  struct PinInterrupt *entry;
+
+  while (current)
+  {
+    listData(list, current, &entry);
+
+    if (entry == interrupt)
+    {
+      listErase(list, current);
+      if (listEmpty(list))
+        irqDisable(calcVector(interrupt->pin.port));
+      break;
+    }
+
+    current = listNext(current);
+  }
+}
+/*----------------------------------------------------------------------------*/
+static enum result pinInterruptHandlerInit(void *object,
+    const void *configBase __attribute__((unused)))
+{
+  struct PinInterruptHandler * const handler = object;
+
+  return listInit(&handler->list, sizeof(struct PinInterrupt *));
+}
+/*----------------------------------------------------------------------------*/
+static enum result pinInterruptInit(void *object, const void *configBase)
+{
+  const struct PinInterruptConfig * const config = configBase;
+  const struct Pin input = pinInit(config->pin);
+  struct PinInterrupt * const interrupt = object;
+  enum result res;
+
+  if (!pinValid(input))
+    return E_VALUE;
+
+  /* Try to register pin interrupt in the interrupt handler */
+  if ((res = pinInterruptHandlerAttach(input.data, interrupt)) != E_OK)
+    return res;
+
+  /* Configure the pin */
+  pinInput(input);
+  pinSetPull(input, config->pull);
+
+  interrupt->callback = 0;
+  interrupt->channel = 0; /* Channel field is left unused */
+  interrupt->event = config->event;
+  interrupt->pin = input.data;
+
+  LPC_GPIO_Type * const reg = calcPort(interrupt->pin.port);
+  const uint32_t mask = 1 << interrupt->pin.offset;
+
+  /* Configure interrupt as edge sensitive*/
+  reg->IS &= ~mask;
+  /* Configure edge sensitivity options */
+  switch (config->event)
+  {
+    case PIN_RISING:
+      reg->IEV |= mask;
+      break;
+
+    case PIN_FALLING:
+      reg->IEV &= ~mask;
+      break;
+
+    case PIN_TOGGLE:
+      reg->IBE |= mask;
+      break;
+  }
+  /* Clear pending interrupt flag */
+  reg->IC = mask;
+  /* Disable interrupt masking */
+  reg->IE |= mask;
+
+  return E_OK;
+}
+/*----------------------------------------------------------------------------*/
+static void pinInterruptDeinit(void *object)
+{
+  const union PinData data = ((struct PinInterrupt *)object)->pin;
+  const uint32_t mask = 1 << data.offset;
+  LPC_GPIO_Type * const reg = calcPort(data.port);
+
+  reg->IE &= ~mask;
+  reg->IBE &= ~mask;
+  reg->IEV &= ~mask;
+  pinInterruptHandlerDetach(object);
+}
+/*----------------------------------------------------------------------------*/
+static void pinInterruptCallback(void *object, void (*callback)(void *),
+    void *argument)
+{
+  struct PinInterrupt * const interrupt = object;
+
+  interrupt->callbackArgument = argument;
+  interrupt->callback = callback;
+}
+/*----------------------------------------------------------------------------*/
+static void pinInterruptSetEnabled(void *object, bool state)
+{
+  const union PinData data = ((struct PinInterrupt *)object)->pin;
+  const uint32_t mask = 1 << data.offset;
+  LPC_GPIO_Type * const reg = calcPort(data.port);
+
+  if (state)
+  {
+    reg->IC = mask;
+    reg->IE |= mask;
+  }
+  else
+    reg->IE &= ~mask;
+}
