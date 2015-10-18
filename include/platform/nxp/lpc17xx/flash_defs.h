bb8875607ae2db790416723586c13a809594897c
xent <alexdmitv@gmail.com>
2015-10-18 17:01:23 +0300

    Added Flash interface for LPC17xx.

---
+++ include/platform/nxp/lpc17xx/flash_defs.h
@@ -0,0 +1,60 @@
+/*
+ * platform/nxp/lpc17xx/flash_defs.h
+ * Copyright (C) 2015 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#ifndef PLATFORM_NXP_LPC17XX_FLASH_DEFS_H_
+#define PLATFORM_NXP_LPC17XX_FLASH_DEFS_H_
+/*----------------------------------------------------------------------------*/
+#include <stdint.h>
+/*----------------------------------------------------------------------------*/
+#define CODE_LPC1769            0x26113F37
+#define CODE_LPC1768            0x26013F37
+#define CODE_LPC1767            0x26012837
+#define CODE_LPC1766            0x26013F33
+#define CODE_LPC1765            0x26013733
+#define CODE_LPC1764            0x26011922
+#define CODE_LPC1763            0x26012033
+#define CODE_LPC1759            0x25113737
+#define CODE_LPC1758            0x25013F37
+#define CODE_LPC1756            0x25011723
+#define CODE_LPC1754            0x25011722
+#define CODE_LPC1752            0x25001121
+#define CODE_LPC1751            0x25001118
+#define CODE_LPC1751_00         0x25001110
+/*----------------------------------------------------------------------------*/
+#define FLASH_PAGE_SIZE         256
+#define FLASH_SECTOR_SIZE_0     4096
+#define FLASH_SECTOR_SIZE_1     32768
+#define FLASH_SECTORS_BORDER    0x10000
+#define FLASH_SECTORS_OFFSET    (FLASH_SECTORS_BORDER / FLASH_SECTOR_SIZE_0 \
+    - FLASH_SECTORS_BORDER / FLASH_SECTOR_SIZE_1)
+/*----------------------------------------------------------------------------*/
+#define IAP_BASE            0x1FFF1FF1U
+/*----------------------------------------------------------------------------*/
+static inline uint8_t addressToBank(uint32_t address __attribute__((unused)))
+{
+  return 0;
+}
+/*----------------------------------------------------------------------------*/
+static inline uint32_t addressToPage(uint32_t address)
+{
+  return address / FLASH_PAGE_SIZE;
+}
+/*----------------------------------------------------------------------------*/
+static inline uint32_t addressToSector(uint32_t address)
+{
+  if (address < FLASH_SECTORS_BORDER)
+  {
+    /* Sectors from 0 to 15 have size of 4 kB */
+    return address / FLASH_SECTOR_SIZE_0;
+  }
+  else
+  {
+    /* Sectors from 16 to 29 have size of 32 kB */
+    return address / FLASH_SECTOR_SIZE_1 + FLASH_SECTORS_OFFSET;
+  }
+}
+/*----------------------------------------------------------------------------*/
+#endif /* PLATFORM_NXP_LPC17XX_FLASH_DEFS_H_ */
---
+++ platform/nxp/lpc17xx/flash.c
@@ -0,0 +1,212 @@
+/*
+ * flash.c
+ * Copyright (C) 2015 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
+#include <platform/nxp/flash.h>
+#include <platform/nxp/lpc17xx/flash_defs.h>
+/*----------------------------------------------------------------------------*/
+static inline bool isPageAddressValid(const struct Flash *, uint32_t);
+static inline bool isSectorAddressValid(const struct Flash *, uint32_t);
+/*----------------------------------------------------------------------------*/
+static enum result flashInit(void *, const void *);
+static void flashDeinit(void *);
+static enum result flashCallback(void *, void (*)(void *), void *);
+static enum result flashGet(void *, enum ifOption, void *);
+static enum result flashSet(void *, enum ifOption, const void *);
+static uint32_t flashRead(void *, uint8_t *, uint32_t);
+static uint32_t flashWrite(void *, const uint8_t *, uint32_t);
+/*----------------------------------------------------------------------------*/
+static const struct InterfaceClass flashTable = {
+    .size = sizeof(struct Flash),
+    .init = flashInit,
+    .deinit = flashDeinit,
+
+    .callback = flashCallback,
+    .get = flashGet,
+    .set = flashSet,
+    .read = flashRead,
+    .write = flashWrite
+};
+/*----------------------------------------------------------------------------*/
+const struct InterfaceClass * const Flash = &flashTable;
+/*----------------------------------------------------------------------------*/
+static inline bool isPageAddressValid(const struct Flash *interface,
+    uint32_t address)
+{
+  return !(address & (FLASH_PAGE_SIZE - 1)) && address < interface->size;
+}
+/*----------------------------------------------------------------------------*/
+static inline bool isSectorAddressValid(const struct Flash *interface,
+    uint32_t address)
+{
+  if (address >= interface->size)
+    return false;
+
+  if (address < FLASH_SECTORS_BORDER)
+    return !(address & (FLASH_SECTOR_SIZE_0 - 1));
+  else
+    return !(address & (FLASH_SECTOR_SIZE_1 - 1));
+}
+/*----------------------------------------------------------------------------*/
+static enum result flashInit(void *object,
+    const void *configBase __attribute__((unused)))
+{
+  struct Flash * const interface = object;
+
+  const uint32_t id = flashReadId();
+
+  switch (id)
+  {
+    case CODE_LPC1751_00:
+    case CODE_LPC1751:
+      interface->size = 32 * 1024;
+      break;
+
+    case CODE_LPC1752:
+      interface->size = 64 * 1024;
+      break;
+
+    case CODE_LPC1754:
+    case CODE_LPC1764:
+      interface->size = 128 * 1024;
+      break;
+
+    case CODE_LPC1756:
+    case CODE_LPC1763:
+    case CODE_LPC1765:
+    case CODE_LPC1766:
+      interface->size = 256 * 1024;
+      break;
+
+    case CODE_LPC1758:
+    case CODE_LPC1759:
+    case CODE_LPC1767:
+    case CODE_LPC1768:
+    case CODE_LPC1769:
+      interface->size = 512 * 1024;
+      break;
+
+    default:
+      return E_ERROR;
+  }
+
+  interface->callback = 0;
+  interface->position = 0;
+
+  return E_OK;
+}
+/*----------------------------------------------------------------------------*/
+static void flashDeinit(void *object __attribute__((unused)))
+{
+
+}
+/*----------------------------------------------------------------------------*/
+static enum result flashCallback(void *object, void (*callback)(void *),
+    void *argument)
+{
+  struct Flash * const interface = object;
+
+  interface->callbackArgument = argument;
+  interface->callback = callback;
+  return E_OK;
+}
+/*----------------------------------------------------------------------------*/
+static enum result flashGet(void *object, enum ifOption option, void *data)
+{
+  struct Flash * const interface = object;
+
+  switch (option)
+  {
+    case IF_ADDRESS:
+      *(uint32_t *)data = interface->position;
+      return E_OK;
+
+    case IF_SIZE:
+      *((uint32_t *)data) = interface->size;
+      return E_OK;
+
+    default:
+      return E_ERROR;
+  }
+}
+/*----------------------------------------------------------------------------*/
+static enum result flashSet(void *object, enum ifOption option,
+    const void *data)
+{
+  struct Flash * const interface = object;
+
+  /* Additional Flash options */
+  switch ((enum flashOption)option)
+  {
+    case IF_FLASH_ERASE_SECTOR:
+    {
+      const uint32_t address = *(const uint32_t *)data;
+
+      if (!isSectorAddressValid(interface, address))
+        return E_VALUE;
+
+      if (flashBlankCheckSector(address) == E_OK)
+        return E_OK;
+
+      return flashEraseSector(address);
+    }
+
+    default:
+      break;
+  }
+
+  switch (option)
+  {
+    case IF_ADDRESS:
+    {
+      const uint32_t position = *(const uint32_t *)data;
+
+      if (!isPageAddressValid(interface, position))
+        return E_VALUE;
+
+      interface->position = position;
+      return E_OK;
+    }
+
+    default:
+      return E_ERROR;
+  }
+}
+/*----------------------------------------------------------------------------*/
+static uint32_t flashRead(void *object, uint8_t *buffer, uint32_t length)
+{
+  struct Flash * const interface = object;
+
+  if (length & (FLASH_PAGE_SIZE - 1))
+    return 0;
+
+  memcpy(buffer, (const void *)interface->position, length);
+
+  if (interface->callback)
+    interface->callback(interface->callbackArgument);
+
+  return length;
+}
+/*----------------------------------------------------------------------------*/
+static uint32_t flashWrite(void *object, const uint8_t *buffer, uint32_t length)
+{
+  struct Flash * const interface = object;
+
+  if (!isSectorAddressValid(interface, interface->position))
+    return 0;
+  if (length & (FLASH_PAGE_SIZE - 1))
+    return 0;
+
+  if (flashWriteBuffer(interface->position, buffer, length) != E_OK)
+    return 0;
+
+  if (interface->callback)
+    interface->callback(interface->callbackArgument);
+
+  return length;
+}
