3c4083672835720f356d5aa96da228d0ca1ac262
xent <alexdmitv@gmail.com>
2020-10-20 23:23:39 +0300

    Added CRP defs for LPC.

---
+++ include/halm/platform/nxp/crp_defs.h
@@ -0,0 +1,15 @@
+/*
+ * halm/platform/nxp/crp_defs.h
+ * Copyright (C) 2020 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#ifndef HALM_PLATFORM_NXP_CRP_DEFS_H_
+#define HALM_PLATFORM_NXP_CRP_DEFS_H_
+/*----------------------------------------------------------------------------*/
+#define CRP_NONE     0xFFFFFFFFUL
+#define CRP_NO_ISP   0x4E697370UL
+#define CRP_LEVEL_1  0x12345678UL
+#define CRP_LEVEL_2  0x87654321UL
+/*----------------------------------------------------------------------------*/
+#endif /* HALM_PLATFORM_NXP_CRP_DEFS_H_ */
