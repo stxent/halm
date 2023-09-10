d91b16b4e9a376ba0f0a169f7c0972eff1fba6c8
xent <alexdmitv@gmail.com>
2023-09-10 23:20:25 +0300

    Minor fixes for LPC, Nuvoton and STM.
    Fixed PM enabling in SDMMC driver for LPC43xx.
    Removed mock pin remap source for STM32F0xx family.
    Fixed FPU setup for Nuvoton M48x.

--- platform/numicro/m48x/startup.c
+++ platform/numicro/m48x/startup.c
@@ -4,9 +4,17 @@
  * Project is distributed under the terms of the MIT License
  */
 
+#ifdef CONFIG_CORE_CORTEX_FPU
+#include <halm/core/cortex/fpu.h>
+#endif
+
 #include <halm/platform/numicro/system.h>
 /*----------------------------------------------------------------------------*/
 void platformStartup(void)
 {
   sysResetBlock(RST_GPIO);
+
+#ifdef CONFIG_CORE_CORTEX_FPU
+  fpuEnable();
+#endif
 }
--- platform/stm32/stm32f0xx/pin_remap.c
+++
@@ -1,9 +0,0 @@
-/*
- * pin_remap.c
- * Copyright (C) 2020 xent
- * Project is distributed under the terms of the MIT License
- */
-
-#include <halm/platform/platform_defs.h>
-
-// TODO Remove
